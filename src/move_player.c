76a1664e938b2d79a94fad40d82ad338a683ba0a
Claudia <claudiagatalavera@gmail.com>
2024-09-16 16:51:36 +0200

    nothing changed

--- src/move_player.c
+++ src/move_player.c
@@ -6,7 +6,7 @@
 /*   By: clagarci <clagarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 12:50:26 by clagarci          #+#    #+#             */
-/*   Updated: 2024/09/16 16:26:13 by clagarci         ###   ########.fr       */
+/*   Updated: 2024/09/16 16:50:40 by clagarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ void	move_up(t_game *game)
 		game->map.collectable--;
 	if (game->map.map[game->map.player_pos.y][game->map.player_pos.x] != 'E')
 		game->map.map[game->map.player_pos.y][game->map.player_pos.x] = 'P';
-	free(game->textures[2]);
-	game->textures[2] = mlx_xpm_file_to_image(game->mlx_ptr, "./possible_sprites/50x50/player_up_50.xpm", &game->width, &game->height);
-	if (!game->textures[2])
-		ft_error(ERROR_FILE);
+	// free(game->textures[2]);
+	// game->textures[2] = mlx_xpm_file_to_image(game->mlx_ptr, "./possible_sprites/50x50/player_up_50.xpm", &game->width, &game->height);
+	// if (!game->textures[2])
+	// 	ft_error(ERROR_FILE);
 	game->counter++;
 }
 void	move_down(t_game *game)
@@ -36,10 +36,10 @@ void	move_down(t_game *game)
 		game->map.collectable--;
 	if (game->map.map[game->map.player_pos.y][game->map.player_pos.x] != 'E')
 		game->map.map[game->map.player_pos.y][game->map.player_pos.x] = 'P';
-	free(game->textures[2]);
-	game->textures[2] = mlx_xpm_file_to_image(game->mlx_ptr, "./possible_sprites/50x50/player_33x50.xpm", &game->width, &game->height);
-	if (!game->textures[2])
-		ft_error(ERROR_FILE);
+	// free(game->textures[2]);
+	// game->textures[2] = mlx_xpm_file_to_image(game->mlx_ptr, "./possible_sprites/50x50/player_33x50.xpm", &game->width, &game->height);
+	// if (!game->textures[2])
+	// 	ft_error(ERROR_FILE);
 	game->counter++;
 }
 
@@ -52,10 +52,10 @@ void	move_left(t_game *game)
 		game->map.collectable--;
 	if (game->map.map[game->map.player_pos.y][game->map.player_pos.x] != 'E')	
 		game->map.map[game->map.player_pos.y][game->map.player_pos.x] = 'P';
-	free(game->textures[2]);
-	game->textures[2] = mlx_xpm_file_to_image(game->mlx_ptr, "./possible_sprites/50x50/player_left_50.xpm", &game->width, &game->height);
-	if (!game->textures[2])
-		ft_error(ERROR_FILE);
+	// free(game->textures[2]);
+	// game->textures[2] = mlx_xpm_file_to_image(game->mlx_ptr, "./possible_sprites/50x50/player_left_50.xpm", &game->width, &game->height);
+	// if (!game->textures[2])
+	// 	ft_error(ERROR_FILE);
 	game->counter++;
 }
 
@@ -68,9 +68,9 @@ void	move_right(t_game *game)
 		game->map.collectable--;
 	if (game->map.map[game->map.player_pos.y][game->map.player_pos.x] != 'E')	
 		game->map.map[game->map.player_pos.y][game->map.player_pos.x] = 'P';
-	free(game->textures[2]);
-	game->textures[2] = mlx_xpm_file_to_image(game->mlx_ptr, "./possible_sprites/50x50/player_right_50.xpm", &game->width, &game->height);
-	if (!game->textures[2])
-		ft_error(ERROR_FILE);
+	// free(game->textures[2]);
+	// game->textures[2] = mlx_xpm_file_to_image(game->mlx_ptr, "./possible_sprites/50x50/player_right_50.xpm", &game->width, &game->height);
+	// if (!game->textures[2])
+	// 	ft_error(ERROR_FILE);
 	game->counter++;
 }
