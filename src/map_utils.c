b8878b3be4dae7089498475c25ccd74abb73cd74
Claudia <clagarci@student.42madrid.com>
2024-09-19 12:53:31 +0200

    normi passed

---
+++ src/map_utils.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clagarci <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 12:46:55 by clagarci          #+#    #+#             */
+/*   Updated: 2024/09/19 12:51:21 by clagarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/so_long.h"
+
+void	*free_map(char **map, int num)
+{
+	int	i;
+
+	i = 0;
+	while (i < num)
+	{
+		free (map[i]);
+		i++;
+	}
+	free (map);
+	return (NULL);
+}
+
+void	print_map(t_map map)
+{
+	int	i;
+
+	i = 0;
+	while (i < map.size.y)
+	{
+		ft_printf("%s\n", map.map[i]);
+		i++;
+	}
+}
+
+void	init_map(t_map *map)
+{
+	map->object = 0;
+	map->exit = 0;
+	map->player = 0;
+	map->player_pos.x = 0;
+	map->player_pos.y = 0;
+	map->exit_pos.x = 0;
+	map->exit_pos.y = 0;
+}
--- src/parse_map.c
+++ src/parse_map.c
@@ -3,42 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   parse_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: clagarci <clagarci@student.42.fr>          +#+  +:+       +#+        */
+/*   By: clagarci <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 12:36:07 by clagarci          #+#    #+#             */
-/*   Updated: 2024/09/18 13:13:26 by clagarci         ###   ########.fr       */
+/*   Updated: 2024/09/19 12:51:13 by clagarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/so_long.h"
 
-void	*free_map(char **map, int num)
-{
-	int	i;
-
-	i = 0;
-	while (i < num)
-	{
-		free (map[i]);
-		i++;
-	}
-	free (map);
-	return (NULL);
-}
-
-void	print_map(t_map map)
-{
-	int	i;
-
-	i = 0;
-	while (i < map.size.y)
-	{
-		ft_printf("%s\n",map.map[i]);
-		i++;
-	}
-}
-
-void	check_walls(t_map *map)
+void	check_char_and_walls(t_map *map)
 {
 	int	i;
 	int	j;
@@ -49,18 +23,24 @@ void	check_walls(t_map *map)
 	{
 		j = -1;
 		while (++j < map->size.x)
+		{
+			if (map->map[i][j] != '1' && map->map[i][j] != '0' && \
+				map->map[i][j] != 'P' && map->map[i][j] != 'E' && \
+				map->map[i][j] != 'C')
+				ft_error(ERROR_MAP_CHARACTERS);
 			if (map->map[0][j] != '1' || map->map[map->size.y - 1][j] != '1' || \
-			 map->map[i][0] != '1' || map->map[i][map->size.x - 1] != '1')
+			map->map[i][0] != '1' || map->map[i][map->size.x - 1] != '1')
 				ft_error(ERROR_MAP_WALLS);
+		}
 	}
 }
 
-void	check_character(char element)
-{
-	if (element != '1' && element != '0' && element != 'P' && \
-	 element != 'E' && element != 'C')
-		ft_error(ERROR_MAP_CHARACTERS);
-}
+// void	check_character(char element)
+// {
+// 	if (element != '1' && element != '0' && element != 'P' &&
+// 	element != 'E' && element != 'C')
+// 		ft_error(ERROR_MAP_CHARACTERS);
+// }
 
 void	count_elements(t_map *map)
 {
@@ -74,7 +54,7 @@ void	count_elements(t_map *map)
 		j = -1;
 		while (++j < map->size.x)
 		{
-			check_character(map->map[i][j]);
+			//check_character(map->map[i][j]);
 			if (map->map[i][j] == 'P')
 			{
 				map->player_pos = (t_vector){j, i};
@@ -86,20 +66,9 @@ void	count_elements(t_map *map)
 				map->exit++;
 			}
 			else if (map->map[i][j] == 'C')
-				map->collectable++;
+				map->object++;
 		}
 	}
-}
-
-void	init_map(t_map *map)
-{
-	map->collectable = 0;
-	map->exit = 0;
-	map->player = 0;
-	map->player_pos.x = 0;
-	map->player_pos.y = 0;
-	map->exit_pos.x = 0;
-	map->exit_pos.y = 0;
 }
 
 void	check_dimensions(t_map *map)
@@ -123,11 +92,10 @@ void	check_dimensions(t_map *map)
 	map->size.x = cols;
 }
 
-t_map	open_map(char *path)
+char	*open_map(char *path)
 {
 	int		file;
 	char	*str;
-	t_map	map;
 	char	*map_aux;
 	char	*aux;
 
@@ -137,39 +105,42 @@ t_map	open_map(char *path)
 	file = open(path, O_RDONLY);
 	if (file == -1)
 		ft_error(ERROR_FILE);
-	while ((str = get_next_line(file)))
+	str = get_next_line(file);
+	while (str)
 	{
 		if (str[0] == '\n')
-			ft_error(ERROR_MAP_SIZE); // para evitar que se lea un salto de linea como una linea
+			ft_error(ERROR_MAP_SIZE);
 		aux = ft_strjoin(map_aux, str);
 		free (map_aux);
 		map_aux = aux;
 		free (str);
+		str = get_next_line(file);
 	}
-	map.map = ft_split(map_aux, '\n');
-	free (map_aux);
 	close(file);
-	return (map);
+	return (map_aux);
 }
 
 t_map	parse_map(char *path)
 {
 	t_map	map;
+	char	*map_aux;
 
-	map = open_map(path);
+	map_aux = open_map(path);
+	map.map = ft_split(map_aux, '\n');
+	free (map_aux);
 	check_dimensions(&map);
 	print_map(map);
 	printf("Cols: %d\nRows: %d\n", map.size.x, map.size.y);
 	init_map(&map);
-	check_walls(&map);
+	check_char_and_walls(&map);
 	count_elements(&map);
 	printf("Player position: %d %d", map.player_pos.x, map.player_pos.y);
 	if (map.player != 1)
 		ft_error(ERROR_MAP_PLAYER);
 	else if (map.exit != 1)
 		ft_error(ERROR_MAP_EXIT);
-	else if (map.collectable < 1)
-		ft_error(ERROR_MAP_COLLECTABLE);
+	else if (map.object < 1)
+		ft_error(ERROR_MAP_OBJECT);
 	check_path(map);
 	return (map);
 }
