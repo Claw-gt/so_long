d70e6a1abdfc899351e9379e4f7568e8e6f7f93a
Claudia <claudiagatalavera@gmail.com>
2024-09-21 20:55:09 +0200

    remove bonus

--- src/move_player.c
+++ src/move_player.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   move_player.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: clagarci <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: clagarci <clagarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 12:50:26 by clagarci          #+#    #+#             */
-/*   Updated: 2024/09/19 13:11:54 by clagarci         ###   ########.fr       */
+/*   Updated: 2024/09/21 20:53:37 by clagarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	move_up(t_game *game)
 {
-	int	h;
-	int	w;
+	// int	h;
+	// int	w;
 
-	h = game->height;
-	w = game->width;
+	// h = game->height;
+	// w = game->width;
 	if (game->map.map[game->map.player_pos.y][game->map.player_pos.x] != 'E')
 		game->map.map[game->map.player_pos.y][game->map.player_pos.x] = '0';
 	game->map.player_pos.y--;
@@ -26,20 +26,20 @@ void	move_up(t_game *game)
 		game->map.object--;
 	if (game->map.map[game->map.player_pos.y][game->map.player_pos.x] != 'E')
 		game->map.map[game->map.player_pos.y][game->map.player_pos.x] = 'P';
-	mlx_destroy_image(game->mlx, game->player);
-	game->player = mlx_xpm_file_to_image(game->mlx, PLAYER_UP_PATH, &w, &h);
-	if (!game->player)
-		ft_error(ERROR_FILE);
+	// mlx_destroy_image(game->mlx, game->player);
+	// game->player = mlx_xpm_file_to_image(game->mlx, PLAYER_UP_PATH, &w, &h);
+	// if (!game->player)
+	// 	ft_error(ERROR_FILE);
 	game->counter++;
 }
 
 void	move_down(t_game *game)
 {
-	int	h;
-	int	w;
+	// int	h;
+	// int	w;
 
-	h = game->height;
-	w = game->width;
+	// h = game->height;
+	// w = game->width;
 	if (game->map.map[game->map.player_pos.y][game->map.player_pos.x] != 'E')
 		game->map.map[game->map.player_pos.y][game->map.player_pos.x] = '0';
 	game->map.player_pos.y++;
@@ -47,20 +47,20 @@ void	move_down(t_game *game)
 		game->map.object--;
 	if (game->map.map[game->map.player_pos.y][game->map.player_pos.x] != 'E')
 		game->map.map[game->map.player_pos.y][game->map.player_pos.x] = 'P';
-	mlx_destroy_image(game->mlx, game->player);
-	game->player = mlx_xpm_file_to_image(game->mlx, PLAYER_DOWN_PATH, &w, &h);
-	if (!game->player)
-		ft_error(ERROR_FILE);
+	// mlx_destroy_image(game->mlx, game->player);
+	// game->player = mlx_xpm_file_to_image(game->mlx, PLAYER_DOWN_PATH, &w, &h);
+	// if (!game->player)
+	// 	ft_error(ERROR_FILE);
 	game->counter++;
 }
 
 void	move_left(t_game *game)
 {
-	int	h;
-	int	w;
+	// int	h;
+	// int	w;
 
-	h = game->height;
-	w = game->width;
+	// h = game->height;
+	// w = game->width;
 	if (game->map.map[game->map.player_pos.y][game->map.player_pos.x] != 'E')
 		game->map.map[game->map.player_pos.y][game->map.player_pos.x] = '0';
 	game->map.player_pos.x--;
@@ -68,20 +68,20 @@ void	move_left(t_game *game)
 		game->map.object--;
 	if (game->map.map[game->map.player_pos.y][game->map.player_pos.x] != 'E')
 		game->map.map[game->map.player_pos.y][game->map.player_pos.x] = 'P';
-	mlx_destroy_image(game->mlx, game->player);
-	game->player = mlx_xpm_file_to_image(game->mlx, PLAYER_LEFT_PATH, &w, &h);
-	if (!game->player)
-		ft_error(ERROR_FILE);
+	// mlx_destroy_image(game->mlx, game->player);
+	// game->player = mlx_xpm_file_to_image(game->mlx, PLAYER_LEFT_PATH, &w, &h);
+	// if (!game->player)
+	// 	ft_error(ERROR_FILE);
 	game->counter++;
 }
 
 void	move_right(t_game *game)
 {
-	int	h;
-	int	w;
+	// int	h;
+	// int	w;
 
-	h = game->height;
-	w = game->width;
+	// h = game->height;
+	// w = game->width;
 	if (game->map.map[game->map.player_pos.y][game->map.player_pos.x] != 'E')
 		game->map.map[game->map.player_pos.y][game->map.player_pos.x] = '0';
 	game->map.player_pos.x++;
@@ -89,9 +89,9 @@ void	move_right(t_game *game)
 		game->map.object--;
 	if (game->map.map[game->map.player_pos.y][game->map.player_pos.x] != 'E')
 		game->map.map[game->map.player_pos.y][game->map.player_pos.x] = 'P';
-	mlx_destroy_image(game->mlx, game->player);
-	game->player = mlx_xpm_file_to_image(game->mlx, PLAYER_RIGHT_PATH, &w, &h);
-	if (!game->player)
-		ft_error(ERROR_FILE);
+	// mlx_destroy_image(game->mlx, game->player);
+	// game->player = mlx_xpm_file_to_image(game->mlx, PLAYER_RIGHT_PATH, &w, &h);
+	// if (!game->player)
+	// 	ft_error(ERROR_FILE);
 	game->counter++;
 }
--- src/print_on_screen.c
+++ src/print_on_screen.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_msg.c                                        :+:      :+:    :+:   */
+/*   print_on_screen.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: clagarci <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: clagarci <clagarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 19:21:28 by clagarci          #+#    #+#             */
-/*   Updated: 2024/09/19 19:26:24 by clagarci         ###   ########.fr       */
+/*   Updated: 2024/09/21 20:53:06 by clagarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,8 @@ void	print_count(t_game game)
 
 	count_string = ft_itoa(game.counter);
 	ft_printf("Moves: %s\n", count_string);
-	print_msg(game, 10, 20, "Moves:");
-	print_img(game, game.wall, TILE_SIZE, 0);
-	print_msg(game, 50, 20, count_string);
+	// print_msg(game, 10, 20, "Moves:");
+	// print_img(game, game.wall, TILE_SIZE, 0);
+	// print_msg(game, 50, 20, count_string);
 	free(count_string);
 }
